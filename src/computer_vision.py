150e2974957a9e89a52cdb1a29d87010698fa5f8
Rivnak, Michael <rivnakm@wit.edu>
2019-04-05 23:37:28 -0400

    Added serial integration

---
+++ src/computer_vision.py
@@ -0,0 +1,16 @@
+# ############################## Start the webserver, the opencv color grabber and the GUI #############################
+
+import start_server
+from threading import Thread
+from vision2 import grab_colors
+background_thread = Thread(target=start_server.start, args=(8080, 20, 2))
+background_thread.start()
+# Server listens now on port 8080, maxlength 20 moves, timeout 2 seconds
+
+thr = Thread(target=grab_colors, args=())
+thr.start()
+# run the opencv code and detect facelet colors
+
+import client_gui2
+# Start the GUI with several sliders to configure some opencv parameters
+
